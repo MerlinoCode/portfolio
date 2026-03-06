f4e4f5872f2595e666fa632f0f53cc2e01798df9
Jonathan Merlino <jonathan.leonel.merlino@gmail.com>
2026-03-06 15:07:15 -0300

    Footer: Deleted extra text.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,26 @@ import Firework from './icons/Firework'
 import { useLanguage } from '../context/LanguageContext';
 
 const Footer = () => {
-  const {translations} = useLanguage();
+  const { translations } = useLanguage();
   function getYear() { return new Date().getFullYear(); }
-  
+
   return (
     <footer>
-      <Firework/>
+      <Firework />
       <p className='footer-text'>
         {translations.footer.first_line[0]}
-        <span> 
-          {getYear()} 
+        <span>
+          {getYear()}
         </span>
         {translations.footer.first_line[1]}
       </p>
       <p className='footer-text'>
         {translations.footer.second_line[0]}
-        {translations.footer.second_line[1]}
         <a href='https://www.figma.com/' target='_blank' rel="noreferrer noopener">Figma</a>
-        {translations.footer.second_line[1]} 
+        {translations.footer.second_line[1]}
         <a href='https://react.dev/' target='_blank' rel="noreferrer noopener">React</a> &
         <a href='https://vitejs.dev/' target='_blank' rel="noreferrer noopener"> Vite</a>
-        </p>
+      </p>
       <p className='footer-text'>{translations.footer.third_line}</p>
     </footer>
   )
